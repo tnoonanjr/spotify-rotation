6e9a77190ccd4cd768dcd1200d3e038eeed7f5c4
Tommy Noonan <tjn4365@gmail.com>
2025-06-14 23:05:50 -0400

    Update auth logic

--- bin/rotation.js
+++ bin/rotation.js
@@ -14,47 +14,43 @@ console.log('Limit:', options.limit);
 console.log('Playlist description:', options.playlistDescription);
 
 let accessToken, tokenData;
-if (fs.existsSync('.spotify_token.json')) {
-    try {
-        const tokenJson = fs.readFileSync('.spotify_token.json', 'utf-8');
-        const cachedTokenData = JSON.parse(tokenJson);
 
-        console.log('Using cached token data...');
-        const refreshToken = cachedTokenData.refresh_token
-
-        tokenData = await refreshAccessToken(refreshToken);
-        accessToken = tokenData.access_token;
-
-    } catch (error) {
-        console.error('An error occurred reading cached token data:', error);
-        process.exit(1);
-    }
-} else if (process.env.SPOTIFY_REFRESH_TOKEN) {
+async function authenticate() {
+  if (process.env.SPOTIFY_REFRESH_TOKEN) {
     console.log('Using environment variables to refresh access token...');
-    tokenData = await refreshAccessToken(process.env.SPOTIFY_REFRESH_TOKEN);
-    accessToken = tokenData.access_token;
-} else {
-    console.log('Starting authentication server...');
-    tokenData = await startServer();
-    accessToken = tokenData.access_token;
+    return await refreshAccessToken(process.env.SPOTIFY_REFRESH_TOKEN);
+  }
+  
+  if (fs.existsSync('.spotify_token.json')) {
+    try {
+      const tokenJson = fs.readFileSync('.spotify_token.json', 'utf-8');
+      const cachedTokenData = JSON.parse(tokenJson);
+      console.log('Using cached token data...');
+      return await refreshAccessToken(cachedTokenData.refresh_token);
+    } catch (error) {
+      console.error('An error occurred reading cached token data:', error);
+    }
+  }
+  
+  console.log('Starting authentication server...');
+  return await startServer();
 }
-if (!tokenData)  {
-    console.error('An error occured retrieving token data.');
-    process.exit(1);
-}
 
-console.log('\n✅ Authentication successful!');
+try {
+  tokenData = await authenticate();
+  accessToken = tokenData.access_token;
+  
+  console.log('\n✅ Authentication successful!');
 
-// Get user profile
-const user = await getUserProfile(accessToken);
-const userId = user.id;
-console.log(`👋 Hello ${user.display_name}`);
+  // Get user profile
+  const user = await getUserProfile(accessToken);
+  const userId = user.id;
+  console.log(`👋 Hello ${user.display_name}`);
 
-
-// Get user playlists
-const playlists = await getUserPlaylists(accessToken, userId);
-let playlistId = null;
-if (options.isUsingExisting) {
+  // Get user playlists
+  const playlists = await getUserPlaylists(accessToken, userId);
+  let playlistId = null;
+  if (options.isUsingExisting) {
     const playlist = playlists.items.find(pl => pl.name === options.playlistName);
     if (!playlist) {
         console.error(`Playlist "${options.playlistName}" not found`);
@@ -63,21 +59,25 @@ if (options.isUsingExisting) {
     playlistId = playlist.id;
 
     console.log(`Using existing playlist: ${options.playlistName}...`);
-} else {
+  } else {
     const playlist = await postUserPlaylist(accessToken, userId, options.playlistName, options.playlistDescription, true);
     playlistId = playlist.id;
     console.log(`Creating playlist: ${playlist.name}...`);
-}
+  }
 
-// Get user top tracks
-const userTopTracks = await getUserTopTracks(accessToken, options.timeRange, options.limit);
-console.log('Adding tracks:', userTopTracks.items.map(track => track.name).join(' | '));
+  // Get user top tracks
+  const userTopTracks = await getUserTopTracks(accessToken, options.timeRange, options.limit);
+  console.log('Adding tracks:', userTopTracks.items.map(track => track.name).join(' | '));
 
-// Add tracks to the playlist
-const trackUris = userTopTracks.items.map(track => track.uri);
-if (options.isUsingExisting) {
+  // Add tracks to the playlist
+  const trackUris = userTopTracks.items.map(track => track.uri);
+  if (options.isUsingExisting) {
     await replacePlaylistItems(accessToken, playlistId, trackUris);
-} else {
+  } else {
     await addToUserPlaylist(accessToken, playlistId, trackUris);
+  }
+  console.log('🚀 Script run successfully!');
+} catch (error) {
+  console.error('Error:', error.message);
+  process.exit(1);
 }
-console.log('🚀 Script run successfully!');
